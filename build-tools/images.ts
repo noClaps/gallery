1a9618ede59d64aa6ff7d3b3760f4b0543210c57
noClaps <github@zerolimits.dev>
2023-12-15 07:19:52 +0000

    Skip optimising images that have already been optimised

--- build-tools/images.ts
+++ build-tools/images.ts
@@ -10,13 +10,21 @@ for (const year of years) {
     const imgFiles = Array.from(glob.scanSync({ cwd: `./src/assets/${ year }` }));
     for (const imgFile of imgFiles) {
         const filename = imgFile.replace(/jpe?g/, "avif");
-        sharp(`./src/assets/${ year }/${ imgFile }`)
-            .toFormat("avif")
-            .toBuffer((_, buffer, info) => {
-                Bun.write(`./dist/images/${ filename }`, buffer);
-                writer.write(`<img src="/images/${ filename }" width="${ info.width }" height="${ info.height }" loading="lazy" decoding="async">`);
+        if (await Bun.file(`./dist/images/${ filename }`).exists()) {
+            await sharp(`./dist/images/${ filename }`).metadata().then((metadata) => {
+                console.log(`${ filename } already exists!`);
+                writer.write(`<img src="/images/${ filename }" width="${ metadata.width }" height="${ metadata.height }" loading="lazy" decoding="async">`);
                 writer.flush();
-                console.log(`Optimised ${ filename }`);
             });
+        } else {
+            sharp(`./src/assets/${ year }/${ imgFile }`)
+                .toFormat("avif")
+                .toBuffer((_, buffer, info) => {
+                    Bun.write(`./dist/images/${ filename }`, buffer);
+                    writer.write(`<img src="/images/${ filename }" width="${ info.width }" height="${ info.height }" loading="lazy" decoding="async">`);
+                    writer.flush();
+                    console.log(`Optimised ${ filename }`);
+                });
+        }
     }
 }
