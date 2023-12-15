9bfe031883427621f0fa97ca6c9c2faf4a4af7c7
noClaps <github@zerolimits.dev>
2023-12-15 10:10:18 +0000

    Merge images and HTML build steps

--- build-tools/html.ts
+++ build-tools/html.ts
@@ -1,9 +1,31 @@
+import { Glob, file, write } from "bun";
+import sharp from "sharp";
+
 const rewriter = new HTMLRewriter();
-rewriter.on("*", {
-    async comments(comment) {
-        const html = await Bun.file(`./tmp/${ comment.text.replaceAll(" ", "") }.html`).text();
-        comment.replace(html, { html: true });
+const glob = new Glob("*.{jpg,jpeg}");
+
+rewriter.on("section", {
+    async element(element) {
+        const year = element.getAttribute("class");
+        const imgFiles = Array.from(glob.scanSync({ cwd: `./src/assets/${ year }` }));
+
+        for (const imgFile of imgFiles) {
+            const filename = imgFile.replace(/jpe?g/, "avif");
+            await sharp(`./dist/images/${ filename }`).metadata().then((metadata) => {
+                console.log(`${ filename } already exists!`);
+                element.append(`<img src="/images/${ filename }" width="${ metadata.width }" height="${ metadata.height }" loading="lazy" decoding="async">`, { html: true });
+            }).catch(() => {
+                sharp(`./src/assets/${ year }/${ imgFile }`)
+                    .toFormat("avif")
+                    .toBuffer((_, buffer, info) => {
+                        Bun.write(`./dist/images/${ filename }`, buffer);
+                        element.append(`<img src="/images/${ filename }" width="${ info.width }" height="${ info.height }" loading="lazy" decoding="async">`, { html: true });
+                        console.log(`Optimised ${ filename }`);
+                    });
+            }
+            );
+        }
     },
 });
 
-Bun.write("./dist/index.html", rewriter.transform(new Response(Bun.file("./src/pages/index.html"))));
+write("./dist/index.html", rewriter.transform(new Response(file("./src/pages/index.html"))));
