dc0111fc103a05969cb4def3b453910a07cfd129
noClaps <github@zerolimits.dev>
2023-12-16 06:42:18 +0000

    Create build

--- build.ts
+++ build.ts
@@ -1,27 +1,42 @@
 import sharp from "sharp";
 
-const rewriter = new HTMLRewriter();
+console.log("Writing CSS...\n");
+Bun.write("./dist/style.css", Bun.file("./src/style.css")).then(() => {
+    console.log("Done writing CSS!\n");
+});
+
+console.log("Copying favicon...\n");
+Bun.write("./dist/favicon.ico", Bun.file("./src/favicon.ico")).then(() => {
+    console.log("Done copying favicon!\n");
+});
+
+const rw = new HTMLRewriter();
 const glob = new Bun.Glob("*.{jpg,jpeg}");
 
-rewriter.on("section", {
-    element(element) {
+rw.on("section", {
+    async element(element) {
         const year = element.getAttribute("class");
-        const images = glob.scanSync({ cwd: `./src/assets/${ year }` });
-        console.log(year);
+        const images = Array.from(glob.scanSync(`./src/assets/${ year }`));
 
-        for (const image of images) {
-            const filename = image.replace(/jpe?g/, "avif");
-            sharp(`./src/assets/${ year }/${ image }`)
-                .toFormat("avif")
-                .toFile(`./dist/images/${ filename }`, (_, info) => {
-                    element.append(`<img src="images/${ filename }" width="${ info.width }" height="${ info.height }" loading="lazy" decoding="async">`, { html: true });
-                    console.log("Optimised image");
-                });
+        for (const img of images) {
+            const filename = img.replace(/jpe?g/, "avif");
+            await sharp(`./dist/images/${ filename }`).metadata().then((metadata) => {
+                console.log(`${ filename } already exists!`);
+                element.append(`<img src="/images/${ filename }" width="${ metadata.width }" height="${ metadata.height }" loading="lazy" decoding="async">`, { html: true });
+            }).catch(() => {
+                sharp(`./src/assets/${ year }/${ img }`)
+                    .toFormat("avif")
+                    .toBuffer((_, buffer, info) => {
+                        Bun.write(`./dist/images/${ filename }`, buffer);
+                        element.append(`<img src="/images/${ filename }" width="${ info.width }" height="${ info.height }" loading="lazy" decoding="async">`, { html: true });
+                        console.log(`Optimised ${ filename }`);
+                    });
+            });
         }
     },
 });
 
-await Bun.write("./dist/index.html", Bun.file("./src/index.html"));
-await Bun.write("./dist/style.css", Bun.file("./src/style.css"));
-await Bun.write("./dist/favicon.ico", Bun.file("./src/favicon.ico"));
-await Bun.write("./src/index.html", rewriter.transform(new Response(await Bun.file("./src/index.html").text())));
+console.log("Building HTML and optimising images...\n");
+Bun.write("./dist/index.html", rw.transform(new Response(Bun.file("./src/index.html")))).then(() => {
+    console.log("Done building HTML and optimising images!\n");
+});
