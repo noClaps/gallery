5eb597db6d3ee9db5502322015b4e8c96afcd0fd
noClaps <github@zerolimits.dev>
2024-01-05 08:20:08 +0000

    Reorganise build-html.ts for fewer crashes

--- scripts/build-html.ts
+++ scripts/build-html.ts
@@ -7,24 +7,29 @@ const rewriter = new HTMLRewriter();
 const html = rewriter.on("section", {
     async element(element) {
         const year = +element.getAttribute("class")!;
-        const images = Array.from(glob.scanSync({ cwd: `./src/assets/${ year }` })).toSorted();
+        const images = Array.from(glob.scanSync({ cwd: `./src/assets/${ year }` })).toSorted().toReversed();
 
         for (const image of images) {
-            let buffer: Buffer;
-
             const hash = Bun.hash(await Bun.file(`./src/assets/${ year }/${ image }`).arrayBuffer());
             const filename = `${ hash }.avif`;
 
             if (!await Bun.file(`./node_modules/_images/${ filename }`).exists()) {
                 console.log("Optimising image:", image);
-                buffer = await sharp(`./src/assets/${ year }/${ image }`).avif().toBuffer();
-                Bun.write(`./node_modules/_images/${ filename }`, buffer);
+                await sharp(`./src/assets/${ year }/${ image }`).avif().toBuffer((err, buffer) => {
+                    if (err) {
+                        console.error(err);
+                    }
+
+                    Bun.write(`./node_modules/_images/${ filename }`, buffer);
+                }).metadata().then((info) => {
+                    element.append(`<img src="/_images/${ filename }" width="${ info.width }" height="${ info.height }" loading="lazy" decoding="async">`, { html: true });
+                });
             } else {
                 console.log("Skipped image:", image);
+                const info = await sharp(`./node_modules/_images/${ filename }`).metadata();
+                element.append(`<img src="/_images/${ filename }" width="${ info.width }" height="${ info.height }" loading="lazy" decoding="async">`, { html: true });
             }
 
-            const info = await sharp(`./node_modules/_images/${ filename }`).metadata();
-            element.append(`<img src="/_images/${ filename }" width="${ info.width }" height="${ info.height }" loading="lazy" decoding="async">`, { html: true });
         }
     }
 });
