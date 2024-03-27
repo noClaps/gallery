292a749514b109044c24c0df10f1b4373a42af0c
noClaps <github@zerolimits.dev>
2024-03-27 13:59:24 +0000

    Add file watching dev script

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,14 +1,28 @@
 import { $ } from "bun";
+import { watch } from "fs";
 
-await $`bun scripts/build.ts`;
+async function serve() {
+  await $`bun scripts/build.ts`;
 
-Bun.serve({
-  fetch(req) {
-    const path = new URL(req.url).pathname;
+  Bun.serve({
+    fetch(req) {
+      const path = new URL(req.url).pathname;
 
-    if (path === "/") return new Response(Bun.file("dist/index.html"));
-    return new Response(Bun.file(`dist${path}`));
-  },
-});
+      if (path === "/") return new Response(Bun.file("dist/index.html"));
+      return new Response(Bun.file(`dist${path}`));
+    },
+    reusePort: true,
+  });
+}
 
+serve();
 console.log("Server running on http://localhost:3000");
+
+watch(
+  `${import.meta.dir}/../src`,
+  { recursive: true },
+  async (event, filename) => {
+    console.log(`Detected ${event} in ${filename}`);
+    serve();
+  },
+);
