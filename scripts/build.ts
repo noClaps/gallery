a3960a86d8bd5e734edf7b7eb6366223b52f81bf
noClaps <github@zerolimits.dev>
2024-06-30 02:00:54 +0100

    Add alts (#9)
    * Remove Docker files and update dependencies
    
    * Update build step
    
    * Add alts to all images
    
    Not the ideal alt text for each image, since it's supposed to help with
    accessibility, but whatever.

--- scripts/build.ts
+++ scripts/build.ts
@@ -6,59 +6,37 @@ await $`cp src/style.css dist`;
 await $`cp src/favicon.ico dist`;
 
 // Build HTML
-const glob = new Bun.Glob("*.{jpg,jpeg,png}");
+await $`mkdir -p node_modules/.cache/_images`;
+const html = new HTMLRewriter().on("img", {
+  async element(el) {
+    el.setAttribute("title", el.getAttribute("alt") ?? "");
+    el.setAttribute("loading", "lazy");
+    el.setAttribute("decoding", "async");
 
-const rewriter = new HTMLRewriter();
-const html = rewriter.on("section", {
-  async element(element) {
-    const year = +element.getAttribute("class")!;
-    const images = Array.from(glob.scanSync({ cwd: `./src/assets/${year}` }))
-      .toSorted()
-      .toReversed();
+    const image = el.getAttribute("src") ?? "";
+    const filename = `${Bun.hash(await Bun.file(image).arrayBuffer())}.avif`;
+    el.setAttribute("src", `/_images/${filename}`);
 
-    for (const image of images) {
-      const hash = Bun.hash(
-        await Bun.file(`./src/assets/${year}/${image}`).arrayBuffer(),
-      );
-      const filename = `${hash}.avif`;
-
-      if (
-        !(await Bun.file(`./node_modules/.cache/_images/${filename}`).exists())
-      ) {
-        console.log("Optimising image:", image);
-        await sharp(`./src/assets/${year}/${image}`)
-          .avif()
-          .toBuffer((err, buffer) => {
-            if (err) {
-              console.error(err);
-            }
-
-            Bun.write(`./node_modules/.cache/_images/${filename}`, buffer);
-          })
-          .metadata()
-          .then((info) => {
-            element.append(
-              `<img src="/_images/${filename}" width="${info.width}" height="${info.height}" loading="lazy" decoding="async">`,
-              { html: true },
-            );
-          });
-      } else {
-        console.log("Skipped image:", image);
-        const info = await sharp(
-          `./node_modules/.cache/_images/${filename}`,
-        ).metadata();
-        element.append(
-          `<img src="/_images/${filename}" width="${info.width}" height="${info.height}" loading="lazy" decoding="async">`,
-          { html: true },
-        );
-      }
+    if (await Bun.file(`node_modules/.cache/_images/${filename}`).exists()) {
+      console.log("Skipped image:", image);
+    } else {
+      console.log("Optimising image:", image);
+      const imageFile = await sharp(image).avif().toBuffer();
+      Bun.write(`node_modules/.cache/_images/${filename}`, imageFile);
     }
+
+    await sharp(image)
+      .metadata()
+      .then((info) => {
+        info.height && el.setAttribute("height", `${info.height}`);
+        info.width && el.setAttribute("width", `${info.width}`);
+      });
   },
 });
 
 Bun.write(
-  "./dist/index.html",
-  html.transform(new Response(await Bun.file("./src/index.html").text())),
+  "dist/index.html",
+  await html.transform(new Response(Bun.file("src/index.html"))).text(),
 );
 
-await $`cp -r node_modules/.cache/_images dist/_images`;
+await $`cp -r node_modules/.cache/_images dist/`;
