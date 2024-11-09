d1ac106f488506efdbe51d792ca481bb0c96515b
noClaps <04plugs-bios@icloud.com>
2024-11-09 20:35:01 +0000

    feat: Bundle CSS with HTML, and minify HTML

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,51 +1,69 @@
-import { $ } from "bun";
 import sharp from "sharp";
 
-await $`mkdir -p dist/_images`;
-await $`cp src/favicon.ico src/style.css dist`;
+Bun.write("src/favicon.ico", Bun.file("src/favicon.ico"));
 
 const rw = new HTMLRewriter();
 
-// Optimise images
-rw.on("img", {
-	async element(el) {
-		const alt = el.getAttribute("alt") ?? "";
-		const image = el.getAttribute("src") ?? "";
+rw.on("link[rel=stylesheet]", {
+  async element(el) {
+    const href = el.getAttribute("href");
+    if (!href) return;
 
-		const imgHash = Bun.hash(await Bun.file(image).arrayBuffer());
-		const filename = `${imgHash}.avif`;
+    const styles = await Bun.build({
+      entrypoints: [`src/${href}`],
+      minify: true,
+      experimentalCss: true,
+    }).then((bo) => bo.outputs[0].text());
 
-		const { height, width } = await sharp(
-			new Uint8Array(await Bun.file(image).arrayBuffer()),
-		).metadata();
-		if (!width || !height) {
-			throw new Error(`Image has no width or height: ${image}`);
-		}
-
-		if (!(await Bun.file(`dist/_images/${filename}`).exists())) {
-			console.log("Optimising image:", image);
-			const buf = await sharp(image).resize(1000).avif().toBuffer();
-			Bun.write(`dist/_images/${filename}`, buf);
-		} else {
-			console.log("Skipped optimising image:", image);
-		}
-
-		const fileExt = Bun.file(image).type.replace("image/", "");
-		const originalFilename = `${imgHash}.${fileExt}`;
-
-		if (!(await Bun.file(`dist/_images/${originalFilename}`).exists())) {
-			console.log("Copying original image:", image);
-			Bun.write(`dist/_images/${originalFilename}`, Bun.file(image));
-		} else {
-			console.log("Skipped copying image:", image);
-		}
-
-		el.replace(
-			`<a href="/_images/${originalFilename}" target="_blank"><img alt="${alt}" title="${alt}" loading="lazy" decoding="async" src="/_images/${filename}" height="${height}" width="${width}"></a>`,
-			{ html: true },
-		);
-	},
+    el.replace(`<style>${styles.trim()}</style>`, { html: true });
+  },
 });
 
-const html = rw.transform(await Bun.file("src/index.html").text());
+// Optimise images
+rw.on("img", {
+  async element(el) {
+    const alt = el.getAttribute("alt") ?? "";
+    const image = el.getAttribute("src") ?? "";
+
+    const imgHash = Bun.hash(await Bun.file(image).arrayBuffer());
+    const filename = `${imgHash}.avif`;
+
+    const { height, width } = await sharp(
+      new Uint8Array(await Bun.file(image).arrayBuffer()),
+    ).metadata();
+    if (!width || !height) {
+      throw new Error(`Image has no width or height: ${image}`);
+    }
+
+    if (!(await Bun.file(`dist/_images/${filename}`).exists())) {
+      console.log("Optimising image:", image);
+      const buf = await sharp(image).resize(1000).avif().toBuffer();
+      Bun.write(`dist/_images/${filename}`, buf);
+    } else {
+      console.log("Skipped optimising image:", image);
+    }
+
+    const fileExt = Bun.file(image).type.replace("image/", "");
+    const originalFilename = `${imgHash}.${fileExt}`;
+
+    if (!(await Bun.file(`dist/_images/${originalFilename}`).exists())) {
+      console.log("Copying original image:", image);
+      Bun.write(`dist/_images/${originalFilename}`, Bun.file(image));
+    } else {
+      console.log("Skipped copying image:", image);
+    }
+
+    el.replace(
+      `<a href="/_images/${originalFilename}" target="_blank"><img alt="${alt}" title="${alt}" loading="lazy" decoding="async" src="/_images/${filename}" height="${height}" width="${width}"></a>`,
+      { html: true },
+    );
+  },
+});
+
+let html = rw.transform(await Bun.file("src/index.html").text());
+html = html
+  .split("\n")
+  .map((l) => l.trim())
+  .join(" ")
+  .replaceAll("> <", "><");
 Bun.write("dist/index.html", html);
