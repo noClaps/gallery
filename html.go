6513fc9df86a46d487a874fa8017d294dcda62e2
noClaps <git@zerolimits.dev>
2025-07-05 15:19:43 +0100

    Extract out `processNode()` function and slighly optimise it
    It's no longer recursive, at least not directly. The node.Descendents()
    function is still recursive internally, but it returns an iterator and
    its not something I can control anyway so whatever.

--- html.go
+++ html.go
@@ -23,93 +23,7 @@ func parseHTML() error {
 	if err != nil {
 		return err
 	}
-	var processNode func(node *html.Node) error
-	processNode = func(node *html.Node) error {
-		if node == nil {
-			return nil
-		}
-
-		for child := range node.ChildNodes() {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				if child.Data == "link" && slices.ContainsFunc(child.Attr, func(attr html.Attribute) bool {
-					return attr.Key == "rel" && attr.Val == "stylesheet"
-				}) {
-					hrefIndex := slices.IndexFunc(child.Attr, func(attr html.Attribute) bool {
-						return attr.Key == "href"
-					})
-					if hrefIndex == -1 {
-						return
-					}
-					href := child.Attr[hrefIndex].Val
-					styles, err := os.ReadFile("src/" + href)
-					if err != nil {
-						log.Println(err)
-					}
-					styleNode, err := html.ParseFragment(strings.NewReader("<style>"+string(styles)+"</style>"), node)
-					if err != nil {
-						log.Println(err)
-					}
-					node.AppendChild(styleNode[0])
-					node.RemoveChild(child)
-				}
-
-				if child.Data == "img" {
-					srcIndex := slices.IndexFunc(child.Attr, func(attr html.Attribute) bool {
-						return attr.Key == "src"
-					})
-					if srcIndex == -1 {
-						return
-					}
-					src := child.Attr[srcIndex].Val
-
-					width, height, fileName, err := parseImage("src/" + src)
-					if err != nil {
-						log.Println(err)
-					}
-					if err := copyFile("dist/_images/"+fileName, ".cache/"+fileName); err != nil {
-						log.Println(err)
-					}
-
-					child.Attr = append(
-						child.Attr,
-						html.Attribute{Key: "width", Val: fmt.Sprint(width)},
-						html.Attribute{Key: "height", Val: fmt.Sprint(height)},
-						html.Attribute{Key: "loading", Val: "lazy"},
-						html.Attribute{Key: "decoding", Val: "async"},
-					)
-					child.Attr[srcIndex] = html.Attribute{Key: "src", Val: "/_images/" + fileName}
-
-					altIndex := slices.IndexFunc(child.Attr, func(attr html.Attribute) bool {
-						return attr.Key == "alt"
-					})
-					if altIndex == -1 {
-						return
-					}
-					alt := child.Attr[altIndex].Val
-
-					child.Attr = append(
-						child.Attr,
-						html.Attribute{Key: "title", Val: alt},
-					)
-				}
-			}()
-
-			for nested := range child.ChildNodes() {
-				err := processNode(nested)
-				if err != nil {
-					return err
-				}
-			}
-		}
-		wg.Wait()
-
-		return nil
-	}
-	if err := processNode(node); err != nil {
-		return err
-	}
+	processNode(node)
 
 	outputFile, err := os.Create("dist/index.html")
 	if err != nil {
@@ -117,4 +31,80 @@ func parseHTML() error {
 	}
 	defer outputFile.Close()
 	return html.Render(outputFile, node)
+}
+
+func processNode(node *html.Node) {
+	for child := range node.Descendants() {
+		if child.Data != "link" && child.Data != "img" {
+			continue
+		}
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if child.Data == "link" && slices.ContainsFunc(child.Attr, func(attr html.Attribute) bool {
+				return attr.Key == "rel" && attr.Val == "stylesheet"
+			}) {
+				hrefIndex := slices.IndexFunc(child.Attr, func(attr html.Attribute) bool {
+					return attr.Key == "href"
+				})
+				if hrefIndex == -1 {
+					return
+				}
+				href := child.Attr[hrefIndex].Val
+				styles, err := os.ReadFile("src/" + href)
+				if err != nil {
+					log.Println(err)
+				}
+				styleNode, err := html.ParseFragment(strings.NewReader("<style>"+string(styles)+"</style>"), child.Parent)
+				if err != nil {
+					log.Println(err)
+				}
+				child.Parent.AppendChild(styleNode[0])
+				child.Parent.RemoveChild(child)
+			}
+
+			if child.Data == "img" {
+				srcIndex := slices.IndexFunc(child.Attr, func(attr html.Attribute) bool {
+					return attr.Key == "src"
+				})
+				if srcIndex == -1 {
+					return
+				}
+				src := child.Attr[srcIndex].Val
+
+				width, height, fileName, err := parseImage("src/" + src)
+				if err != nil {
+					log.Println(err)
+				}
+				if err := copyFile("dist/_images/"+fileName, ".cache/"+fileName); err != nil {
+					log.Println(err)
+				}
+
+				child.Attr = append(
+					child.Attr,
+					html.Attribute{Key: "width", Val: fmt.Sprint(width)},
+					html.Attribute{Key: "height", Val: fmt.Sprint(height)},
+					html.Attribute{Key: "loading", Val: "lazy"},
+					html.Attribute{Key: "decoding", Val: "async"},
+				)
+				child.Attr[srcIndex] = html.Attribute{Key: "src", Val: "/_images/" + fileName}
+
+				altIndex := slices.IndexFunc(child.Attr, func(attr html.Attribute) bool {
+					return attr.Key == "alt"
+				})
+				if altIndex == -1 {
+					return
+				}
+				alt := child.Attr[altIndex].Val
+
+				child.Attr = append(
+					child.Attr,
+					html.Attribute{Key: "title", Val: alt},
+				)
+			}
+
+		}()
+	}
+	wg.Wait()
 }
